e06fb034343e3107546fce25f7cac3541f45c219
Ivan <ivanich86@gmail.com>
2014-01-08 22:57:10 +0200

    feat(calculator): add view and controller
    using angular
    4 inputs and one result field
    
    time: 0h:45

---
+++ src/js/calorie.js
@@ -0,0 +1,35 @@
+﻿function CalorieCtrl($scope) {
+  /** @const */
+  var params = {
+    m: {
+        startIndex: 66.5,
+        weightIndex: 13.8,
+        growthIndex: 5,
+        ageIndex: 6.8
+    },
+    w: {
+        startIndex: 655.1,
+        weightIndex: 9.6,
+        growthIndex: 1.8,
+        ageIndex: 4.7
+    }
+  };
+  
+  var kgInPound = 0.453,
+    smInInch = 2.54;
+
+  $scope.sex;
+  $scope.weight;
+  $scope.age;
+  $scope.growth;
+
+  $scope.result = function(){
+    if ($scope.sex && $scope.weight && $scope.age && $scope.growth){
+        var uniParams = params[$scope.sex];
+        return uniParams.startIndex + 
+        ($scope.weight * kgInPound * uniParams.weightIndex) + 
+        ($scope.growth * smInInch * uniParams.growthIndex) -
+        ($scope.age * uniParams.ageIndex);
+    }
+  };
+}
